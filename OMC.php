cc49a643989f57c61ef7870d4fa35ae973b775c4
polodev <polodev10@gmail.com>
2019-01-20 02:55:10 +0600

    completed omc-data schengen

--- OMC.php
+++ OMC.php
@@ -1,6 +1,26 @@
-<?php 
+<?php
+
+
+
 
 class OMC {
+  const coutry_type = [
+    'schengen',
+    'schengen_us_canada_worldwide',
+    'worldwide',
+  ];
+  const age_group= [
+    'till_40' => [],
+    '41_to_50' => [],
+    '51_to_55' => [],
+    '56_to_59' => [],
+    '60_to_65' => [],
+    '66_to_75' => [],
+    '76_to_80' => [],
+  ];
+  const days = [
+    7, 10, 14, 21, 28, 35, 47, 60, 75, 90, 120, 180
+  ];
   public function __construct()
   {
     
